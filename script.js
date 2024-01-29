952712d0b986ac3d031f8fea82adf291b79db382
Priyanshu Tyagi <110727059+Priyanshu282002@users.noreply.github.com>
2024-01-29 19:38:07 +0530

    Addes CSS File and marked as complete

--- script.js
+++ script.js
@@ -28,18 +28,22 @@ let password="";
 let passwordLength=10;
 let checkCount=0;
 //set strength circle color to grey
-
+setIndicator('#ccc');
 handleSlider();
 
 //set password length
 function handleSlider() {
     inputSlider.value=passwordLength;
     lengthDisplay.innerText=passwordLength;
+    const min=inputSlider.min;
+    const max=inputSlider.max;
+    inputSlider.style.backgroundSize=((passwordLength-min)*100/(max-min)) + "% 100%";
 }
 
 function setIndicator(color){
     indicator.style.backgroundColor=color;
     //shadow -hw
+    indicator.style.boxShadow =`0px 0px 12px 1px ${color}` ;
 }
 
 function getRndInteger(min,max){
@@ -107,10 +111,12 @@ async function copyContent(){
 
 function shufflePassword(array){
 
-    //fisher Yates Method
+    //Fisher Yates Method
     for(let i=array.length-1;i>0;i--)
     {
+        //random j find out using random function
         const j=Math.floor(Math.random()*(i+1));
+        //Swap number at i index and j index
         const temp=array[i];
         array[i]=array[j];
         array[j]=temp;
